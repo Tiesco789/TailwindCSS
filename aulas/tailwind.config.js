03b32636bbd3ac140e56b43605bd16be0ffbde51
Franccesco <franccesco_@hotmail.com>
2022-02-15 14:20:46 -0300

    customização e spotify-homepage clone

--- aulas/tailwind.config.js
+++ aulas/tailwind.config.js
@@ -1,7 +1,20 @@
+const { colors } = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "new-blue": "#243c5a",
+        gray: {
+          ...colors.gray,
+          900: "#999",
+        },
+      },
+      spacing: {
+        50: "20rem",
+      },
+    },
   },
   plugins: [],
 };
