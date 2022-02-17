4056170d5a87cab7a20fb90e473bb7acceff97d1
Franccesco <franccesco_@hotmail.com>
2022-02-17 10:04:52 -0300

    amazon-clone

--- aulas/tailwind.config.js
+++ aulas/tailwind.config.js
@@ -1,16 +1,10 @@
 const { colors } = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        arial: ["Arial", "sans-serif"],
-      },
-      fontSize: {
-        "12xl": "8rem",
-      },
-    },
-  },
+  // purge: {
+  //   enabled: true,
+  // },
+  // content: ["./src/**/*.{html,js}"],
+  theme: {},
   plugins: [],
 };
